4f2a24d940d508bb1cf01498ad86537e157320eb
Hector Valls Mira <hectorvallsmira2003@gmail.com>
2022-03-16 17:02:16 +0100

    [MOD] Extraer atributos a constantes

--- src/main/java/ed/hector/Institut.java
+++ src/main/java/ed/hector/Institut.java
@@ -2,14 +2,19 @@ package ed.hector;
 
 public class Institut {
 
+	private static final int ANY = 2021;
+	private static final String CICLO_FORMATIVO = "1º DAM";
+	private static final String COGNOMS_ALUMNE = "Valls";
+	private static final String NOM_ALUMNE = "Hector";
+
 	public static void main(String[] args) {
 		
 		Alumne p = new Alumne();
 		
-		p.nom = "Hector";
-		p.cognoms = "Valls";
-		p.cicleFormatiu = "1º DAM";
-		p.any = 2021;
+		p.nom = NOM_ALUMNE;
+		p.cognoms = COGNOMS_ALUMNE;
+		p.cicleFormatiu = CICLO_FORMATIVO;
+		p.any = ANY;
 		
 		System.out.println(p.toString());
 
